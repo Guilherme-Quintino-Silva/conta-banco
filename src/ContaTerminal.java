3734f4ead6d9f35292befde3bc103b543f24c5d7
Guilherme-Quintino-Silva <guilherme.luiz.quintino@gmail.com>
2024-05-12 14:01:47 -0300

    feature: Conta bancaria

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    Integer numero;
-    String agencia;
-    String nomeCliente;
-    Double Saldo;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da conta:");
+        Integer numero = scanner.nextInt();
+
+        scanner.nextLine(); // Consumir a linha em branco
+
+        System.out.println("Digite a agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o nome do cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o saldo em conta:");
+        Double saldo = scanner.nextDouble();
+
+        System.out.println("Seu número da conta: " + numero);
+        System.out.println("Seu número da agência: " + agencia);
+        System.out.println("Seu nome do cliente: " + nomeCliente);
+        System.out.println("Seu saldo em conta: " + saldo);
+
+        scanner.close();
+    }
 }
